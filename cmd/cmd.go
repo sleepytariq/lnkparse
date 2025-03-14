8524ae0b72e06f73050e02f085e66ea151015a40
Tariq Alashaikh <105686563+sleepytariq@users.noreply.github.com>
2025-03-15 02:42:44 +0300

    refactor: Move Cmd func to cmd package

---
+++ cmd/cmd.go
@@ -0,0 +1,103 @@
+package cmd
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"lnkparse/lnk"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const version string = "0.2.0"
+
+var flags struct {
+	Trim    bool
+	Json    bool
+	Version bool
+}
+
+func usage() {
+	fmt.Println(`Usage:
+  lnkparse [flags] <PATH>
+
+Examples:
+  lnkparse C:\Users\user\Desktop\file.lnk
+  lnkparse C:\Users\user\Desktop\*.lnk
+  lnkparse C:\Users\user\**\*.lnk
+
+Flags:
+  -trim        trim leading/trailing spaces in command line
+  -json        show output in JSON format
+  -h, -help    show this message and exit
+  -version     show version and exit`)
+}
+
+func Run() {
+	flag.BoolVar(&flags.Trim, "trim", false, "trim leading/trailing spaces in command line")
+	flag.BoolVar(&flags.Json, "json", false, "show output in JSON format")
+	flag.BoolVar(&flags.Version, "version", false, "show version and exit")
+	flag.Usage = usage
+	flag.Parse()
+
+	if flags.Version {
+		fmt.Printf("lnkparse %s\n", version)
+		os.Exit(0)
+	}
+
+	var targetFiles []string
+
+	for _, arg := range flag.CommandLine.Args() {
+		matches, err := filepath.Glob(arg)
+		if err != nil {
+			fmt.Printf("Error: failed to find matches for %s\n", arg)
+			continue
+		}
+		targetFiles = append(targetFiles, matches...)
+	}
+
+	if len(targetFiles) == 0 {
+		fmt.Println("Error: no valid files were passed")
+		os.Exit(1)
+	}
+
+	var lnks []*lnk.Lnk
+
+	for _, targetFile := range targetFiles {
+		l, err := lnk.ParseFromFile(targetFile)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+			continue
+		}
+		lnks = append(lnks, l)
+	}
+
+	if flags.Trim {
+		for _, l := range lnks {
+			l.DataStrings.Arguments = strings.Trim(l.DataStrings.Arguments, " ")
+		}
+	}
+
+	if flags.Json {
+		var jsonData []byte
+		var err error
+		if len(lnks) == 1 {
+			jsonData, err = json.Marshal(lnks[0])
+		} else {
+			jsonData, err = json.Marshal(lnks)
+		}
+		if err != nil {
+			fmt.Println("Error: failed to convert to JSON")
+			os.Exit(1)
+		}
+		fmt.Println(string(jsonData))
+	} else {
+		for i, l := range lnks {
+			fmt.Println(l)
+			if i < len(lnks)-1 && len(lnks) != 1 {
+				fmt.Println(strings.Repeat("-", 48))
+			}
+		}
+	}
+}
